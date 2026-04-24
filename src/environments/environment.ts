939012c39714ffbd05239661c1f0ce0ee7ebed5e
urquidijo <josueurquidicarrillo@gmail.com>
2026-04-23 20:59:25 -0400

    despliegue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,8 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:8080/api'
+// };
+
 export const environment = {
   production: false,
   apiUrl: 'http://3.145.0.100:8080/api'
