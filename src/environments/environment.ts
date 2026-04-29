e6b0da1bd31838351fcdc749e6d70d8ff2a2ee87
urquidijo <josueurquidicarrillo@gmail.com>
2026-04-28 20:52:19 -0400

    url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api',
-  wsUrl: 'wss://aws-docker-production.up.railway.app/ws'
-};
-
 // export const environment = {
 //   production: false,
-//   apiUrl: 'https://aws-docker-production.up.railway.app/api',
+//   apiUrl: 'http://localhost:8080/api',
 //   wsUrl: 'wss://aws-docker-production.up.railway.app/ws'
 // };
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://aws-docker-production.up.railway.app/api',
+  wsUrl: 'wss://aws-docker-production.up.railway.app/ws'
+};
